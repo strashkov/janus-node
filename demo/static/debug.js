90f52a46bc1a07b69f24492040ea2f85360b3024
av.strashkov <av.strashkov@tensor.ru>
2019-07-03 19:00:57 +0300

    debug scroll

--- demo/static/debug.js
+++ demo/static/debug.js
@@ -2,6 +2,7 @@ const area = document.getElementById('debug');
 
 export function debug(text) {
   area.value += `${text}\n\n`;
+  area.scrollTop = area.scrollHeight;
 }
 export function clear() {
   area.value = '';
